ad3fc55748f4282a5c4e784cb95bdbb9d7c7067f
PM36coder <pravesh63362pk@gmail.com>
2025-08-21 08:42:30 +0530

    connected with frontend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.use(cors({
     origin: [
-        "http://localhost:5173",
+        "https://passmanager36.netlify.app/"
         
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Add this
